e8af5d999f6bddbb6acef7ab12eada204dc85683
chiamaka-igbokwe <chiamakaigbokwex@gmail.com>
2025-07-16 19:23:11 +0100

    latest frontend commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div> 
+      <p className="text-center">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App
