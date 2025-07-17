47735621d3b2e740fba26b9fe04be32d6ed2c48e
chiamaka-igbokwe <chiamakaigbokwex@gmail.com>
2025-07-17 21:50:24 +0100

    latest commit

--- Frontend/src/component/Worker.tsx
+++ Frontend/src/component/Worker.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Worker() {
+
+  useEffect(() =>{
+    axios.get("http://localhost:5000/");
+  }, [])
+
   return (
-    <div>
-     <h1>hi</h1> 
+    <div className='flex flex-col items-center justify-center h-min-sceen bg-blue p-4'>
+     <div className='bg-white rounded'>
+      <button className='bg-blue-500'>Add +</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+          </thead>
+          <tbody>
+          </tbody>     
+         </table>
+      </div> 
     </div>
   )
 }
