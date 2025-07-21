d497b4214bbf81fffce0d02cd5e19c97f306f3e6
chiamaka-igbokwe <chiamakaigbokwex@gmail.com>
2025-07-21 18:14:20 +0100

    new commit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,7 +6,6 @@ function App() {
 
   return (
     <div className='App'> 
-    <Worker />
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Worker />}>
--- Frontend/src/component/Worker.tsx
+++ Frontend/src/component/Worker.tsx
@@ -4,25 +4,13 @@ import axios from 'axios';
 
 function Worker() {
 
-  useEffect(() =>{
-    axios.get("http://localhost:5000/");
-  }, [])
+  // useEffect(() =>{
+  //   axios.get("http://localhost:5000/");
+  // }, [])
 
   return (
-    <div className='flex flex-col items-center justify-center h-min-sceen bg-blue p-4'>
-     <div className='bg-white rounded'>
-      <button className='bg-blue-500'>Add +</button>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-          </thead>
-          <tbody>
-          </tbody>     
-         </table>
-      </div> 
+    <div className='flex flex-col items-center justify-center h-min-screen bg-blue p-4'>
+     <h1>Hello dearie</h1>
     </div>
   )
 }
