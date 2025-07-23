752d7966cb9c1bfe66a1172330a2d7539a5cdae6
chiamaka-igbokwe <chiamakaigbokwex@gmail.com>
2025-07-23 14:52:02 +0100

    latest commit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import Worker from "./component/Worker"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Worker from "./component/Worker";
 
 function App() {
-
   return (
     <div className='App'> 
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Worker />}>
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Worker />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
--- Frontend/src/component/Worker.tsx
+++ Frontend/src/component/Worker.tsx
@@ -1,28 +1,24 @@
- import React from 'react'
- import { useEffect, useState } from 'react';
- import axios from 'axios';
- 
 
- function Worker() {
+ function Workers() {
 
- useEffect(() =>{
-   axios.get("http://localhost:5000/");
- }, [])
 
    return (
      <div className="flex flex-col items-center justify-center min-h-screen bg-blue-500 p-4">
   <div className="w-[75%] bg-white rounded p-4 shadow-lg">
-    <button className="btn btn-success mb-4" >Add +</button>
-
+    <button className="bg-green-400 rounded p-2 my-4" >Add +</button>
     <table className="w-full">
       <thead>
         <tr>
           <th className="text-left pb-2">Name</th>
           <th className="text-left pb-2">Email</th>
+          <th className="text-left pb-2">Action</th>
         </tr>
       </thead>
       <tbody>
-        
+        <tr>
+          <td className="border-b-4">Name</td>
+          <td className="border-b-4">Name</td>
+        </tr>
       </tbody>
     </table>
     
@@ -32,5 +28,5 @@
    )
  }
 
- export default Worker
+ export default Workers
 
